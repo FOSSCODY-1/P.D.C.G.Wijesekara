1e91c3e2e54aebcaca690b51b8e6ce1478ee5d4f
chethani76 <chethaniwijesekara@gmail.com>
2018-09-27 11:03:21 +0530

    DFS

--- GraphImplementation/src/graphimplementation/GraphImplementation.java
+++ GraphImplementation/src/graphimplementation/GraphImplementation.java
@@ -4,11 +4,13 @@ package graphimplementation;
 import java.io.*;
 import java.util.*;
 
-//Declaration of class
+
+
+//This class represent a directed graph using adjacency list representation
 public class GraphImplementation {
 
     private int V; //Number of vertices
-    private LinkedList<Integer> graph[]; //adjacency list, declaring a linked list array
+    private LinkedList<Integer> graph[]; //array list for adjacency list
     
     //constructor
     public GraphImplementation(int v) {
@@ -23,7 +25,7 @@ public class GraphImplementation {
     
     //Adding an edge into the graph
     void addEdge(int tail, int head ){
-        graph[tail].add(head);
+        graph[tail].add(head); // add head to tail's list
     }
     
     //Breadth First Traversal from a given vertex s
@@ -59,7 +61,39 @@ public class GraphImplementation {
                 }
             }
         }
-    }  
+    } 
+    
+    //Deapth First Search traversal
+    
+    //recursive util function used in DFS traversal
+    void DFSUtil(int v, boolean visited[])
+    {
+        //Mark the current node as visited and print it
+        visited[v]=true;
+        System.out.println(v+" ");
+        
+        //Traverse the list in either direction, modify the list and
+        //obtain the iterators current position in the list
+        Iterator<Integer> i=graph[v].listIterator();
+        while(i.hasNext())
+        {
+            int n=i.next();
+            if(!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+    
+    //The DFS traversal Function. Uses DFSUtil() recursively
+    void DFS(int v)
+    {
+        //Mark all the vertices as visited, false by default
+        boolean visited[]=new boolean[V];
+        
+        //recursive call DFSUtil for every vertex
+        DFSUtil(v, visited);
+    }
+    
+    
     
     
     //Main function
@@ -75,8 +109,11 @@ public class GraphImplementation {
         g.addEdge(3, 3);
         
         System.out.println("Following is Breadth First Traversal "+"(starting from vertex 2)");
-                           
-        g.BFS(2);
+        g.BFS(2);    
+        
+        System.out.println("Following is Depth First Traversal "+"(starting from vertex 2)");
+        g.DFS(2);                   
+        
         
     }
 }
